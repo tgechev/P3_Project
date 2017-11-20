cc0a8747cbfcbe8a4898827eccb1f17012d55ce2
Konrol <kmatyn16@student.aau.dk>
2017-11-20 13:23:12 +0100

    Images Correct / Wrong made to be read by game

--- Game/Game/GUI.cpp
+++ Game/Game/GUI.cpp
@@ -40,8 +40,8 @@ vector<int> cardResultsPositive = { 1,1,1,1 };
 string winName = "EXCITING GAME";
 
 Mat img;
-Mat correct = imread("images/correct.png", CV_LOAD_IMAGE_COLOR);
-Mat wrong = imread("images/wrong.png", CV_LOAD_IMAGE_COLOR);
+Mat correct = imread("images/Correct.png", CV_LOAD_IMAGE_COLOR);
+Mat wrong = imread("images/Wrong.png", CV_LOAD_IMAGE_COLOR);
 
 void settext(Mat img, int lvl) {
 	string text = "";
